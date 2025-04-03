e50198a87a110c14e372a1bd510296d20a84ed75
Merijn <merijn@ponzo.nl>
2025-04-03 23:46:43 +0200

    working recipe

--- src/main.js
+++ src/main.js
@@ -1,70 +1,33 @@
-import { Recipe } from './recipe';
+import { Recipe } from "./recipe.js";
 
+const tabsBind = new Recipe(".tabs-bind");
 
-new Recipe({store: {flush: ['basket']}}, [
-    {
-      selector: '.tab',
-      steps: [
-        { do: 'click', then: 'tw', to: '.tab-content', add: 'hidden' },
-        { then: 'tw', to: '.tab', remove: 'bg-blue-500', add: 'bg-gray-200' },
-        { then: 'tw', remove: 'hidden' },
-        { then: 'tw', add: 'bg-blue-500', remove: 'bg-gray-200' }
-      ]
-    },
-    {
-      selector: '.add-to-cart',
-      steps: [
-        {
-          do: 'click',
-          then: 'store',
-          store: 'basket',
-          action: 'add',
-          value: { id: 1, name: 'Awesome Product' }
-        },
-        {
-          script: () => {
-            const count = Recipe.store.get('basket', 'length');
-            console.log('script!')
-            Recipe.text('count', count);
-          }
-        },
-        { then: 'tw', to: '.toast', remove: 'hidden' },
-        { then: 'text', to: '.toast .message', text: 'Added "Awesome Product" to cart!' },
-        { wait: 40 },
-        { then: 'tw', to: '.toast', add: 'hidden' },
-      ]
-    },
-    {
-      selector: '.bookmark',
-      steps: [
-        {
-          do: 'click',
-          actions: [
-            {
-                then: 'store',
-                store: 'bookmarks',
-                action: 'toggle',
-                value: Recipe.data.get()
-            },
-            { 
-                then: 'tw', 
-                to: '.toast', 
-                toggle: 'hidden' 
-            },
-          ]
-        },
-        {
-            do: 'mouseover',
-            actions: [
-              { 
-                  then: 'tw', 
-                  to: '.toast', 
-                  toggle: 'ring-4 scale-75' 
-              },
-            ]
-          },
-         { then: 'tw', to: '.bookmark', add: 'text-yellow-500' }
-      ]
-    }
-  ]);
-  
+tabsBind
+  .click(".tab")((index, R) => {
+    R.q(".tab").class.remove("ring-2");
+    R.class.add("ring-2");
+  })
+  .label("tab-click");
+
+tabsBind.bind("tab-click", (index, R) => {
+  R.q(".tab").class.remove("active");
+  R.q(".tab").eq(index).class.add("active");
+
+  R.q(".tab-contents > div").class.add("hidden");
+  R.q(".tab-contents > div").eq(index).class.remove("hidden");
+
+  R.parent.data.set("active", index);
+});
+
+const tabsWatch = new Recipe(".tabs-watch");
+
+tabsWatch.click(".tab")((index, R) => {
+  R.q(".tab").class.remove("active");
+  R.class.add("active");
+  R.parent.data.set("active", index);
+});
+
+tabsWatch.watch("active", (value, R) => {
+  R.q(".tab-contents > div").class.add("hidden");
+  R.q(".tab-contents > div").eq(value).class.remove("hidden");
+});
--- src/recipe.js
+++ src/recipe.js
@@ -1,172 +1,120 @@
 export class Recipe {
-  constructor(config = {}, scenarios = []) {
-    this.state = {};
-    this.scenarios = scenarios;
-    this.config = config;
+  constructor(selector) {
+    this.root = document.querySelector(selector);
+    this.queue = [];
+    this.bindings = {};
+    this.watchers = {};
+    this._state = {};
 
-    if (config?.store?.flush?.length) {
-      config.store.flush.forEach(key => localStorage.removeItem(key));
-    }
-
-    this.bindAllFromSelectors();
-    this.bindAllFromDataAttribute();
-  }
-
-  bindAllFromSelectors() {
-    this.scenarios.forEach(({ selector, steps }) => {
-      document.querySelectorAll(selector).forEach((el) => {
-        this.bindScenario(el, steps);
-      });
-    });
-  }
-
-  bindAllFromDataAttribute() {
-    document.querySelectorAll('[data-recipe]').forEach((el) => {
-      try {
-        const steps = JSON.parse(el.getAttribute('data-recipe'));
-        this.bindScenario(el, steps);
-      } catch (e) {
-        // silently ignore
-      }
-    });
-  }
-
-  bindScenario(el, steps) {
-    steps.forEach((step) => {
-      if (!step.do) return;
-      const actions = step.actions || [step];
-      el.addEventListener(step.do, () => {
-        console.log(`[Recipe] '${step.do}' triggered on`, el);
-        this.runSteps(el, actions);
-      });
-      
-    });
-  }
-
-  async runSteps(el, steps) {
-    for (const step of steps) {
-      if (step.wait) {
-        await this.wait(step.wait);
-      } else if (step.script) {
-        await this.runScript(step.script);
-      } else if (step.then) {
-        await this.runAction(el, step);
-        if (step.id && step.value !== undefined) {
-          this.state[step.id] = step.value;
-          Recipe.text(step.id, step.value);
-        }
-      }
-    }
-  }
-
-  async runScript(refOrFn) {
-    if (typeof refOrFn === 'function') {
-      await refOrFn(this.state, localStorage);
-    } else if (typeof refOrFn === 'string' && window.scriptHandlers?.[refOrFn]) {
-      await window.scriptHandlers[refOrFn](this.state, localStorage);
-    }
-
-    Object.entries(this.state).forEach(([id, value]) => {
-      Recipe.text(id, value);
-    });
-  }
-
-  async runAction(el, step) {
-    let target = el;
-    if (step.to) {
-      target = el.querySelector(step.to) || document.querySelector(step.to);
-    } else if (el.dataset.target) {
-      target = document.querySelector(el.dataset.target);
-    }
-
-    switch (step.then) {
-      case 'tw':
-        if (step.toggle) target?.classList.toggle(...step.toggle.split(' '));
-        if (step.add) target?.classList.add(...step.add.split(' '));
-        if (step.remove) target?.classList.remove(...step.remove.split(' '));
-        if (step.tw) target?.classList.add(...step.tw.split(' '));
-        break;
-
-      case 'store':
-        Recipe.store.set(step.store, step.action, step.value);
-        break;
-
-      case 'text':
-        if (step.text) target.textContent = step.text;
-        break;
-    }
-
-    if (step.id && step.value !== undefined) {
-      this.state[step.id] = step.value;
-      Recipe.text(step.id, step.value);
-    }
-  }
-
-  wait(ms) {
-    return new Promise(res => setTimeout(res, ms));
-  }
-
-  static text(id, value) {
-    document.querySelectorAll(`[data-recipe\\.get="${id}"]`).forEach((el) => {
-      el.textContent = value;
-    });
-  }
-
-  static store = {
-    get(key, prop) {
-      try {
-        const data = JSON.parse(localStorage.getItem(key)) || [];
-        return prop !== undefined ? data?.[prop] : data;
-      } catch (e) {
-        console.warn(`Recipe.store.get(${key}) failed`, e);
-        return undefined;
-      }
-    },
-
-    set(key, action, item) {
-      let value = [];
-      try {
-        value = JSON.parse(localStorage.getItem(key)) || [];
-      } catch (e) {}
-
-      switch (action) {
-        case 'add':
-          value.push(item);
-          break;
-        case 'delete':
-          value = value.filter(p => p.id !== item.id);
-          break;
-        case 'update':
-          value = value.map(p => (p.id === item.id ? item : p));
-          break;
-        case 'toggle': {
-          const index = value.findIndex(p => p.id === item.id);
-          if (index > -1) {
-            value.splice(index, 1);
-          } else {
-            value.push(item);
+    // Reactive store
+    this.store = new Proxy(
+      {},
+      {
+        set: (obj, key, value) => {
+          obj[key] = value;
+          if (this.watchers[key]) {
+            const runtime = this._getRuntime(this.root);
+            this.watchers[key](value, runtime);
           }
-          break;
-        }
+          return true;
+        },
+        get: (obj, key) => obj[key],
       }
+    );
 
-      localStorage.setItem(key, JSON.stringify(value));
-    },
+    // Run setup after DOM is ready
+    setTimeout(() => this._runQueue(), 0);
+  }
 
-    delete(key) {
-      localStorage.removeItem(key);
+  click(selector) {
+    const ctx = this;
+
+    const wrapper = (handler) => {
+      ctx.queue.push(() => {
+        const elements = ctx.root.querySelectorAll(selector);
+        elements.forEach((el, index) => {
+          el.addEventListener("click", () => {
+            const R = ctx._getRuntime(el);
+            handler(index, R); // Always pass index, R
+            if (wrapper._label) {
+              ctx._dispatch(wrapper._label, index, R);
+            }
+          });
+        });
+      });
+      return wrapper;
+    };
+
+    wrapper.label = function (labelName) {
+      wrapper._label = labelName;
+      return wrapper;
+    };
+
+    return wrapper;
+  }
+
+  bind(label, handler) {
+    this.bindings[label] = (payload, R) => {
+      handler(payload, R);
+    };
+  }
+
+  watch(key, handler) {
+    this.watchers[key] = (value, R) => {
+      handler(value, R);
+    };
+  }
+
+  _dispatch(label, payload, R) {
+    if (this.bindings[label]) {
+      this.bindings[label](payload, R);
     }
-  };
+  }
 
-  static data = {
-    get(ref) {
-      if (ref) {
-        const el = document.querySelector(`[data-recipe.get="${ref}"]`);
-        return el?.textContent;
-      }
-      return this?.textContent || '';
-    }
-  };
+  _runQueue() {
+    this.queue.forEach((fn) => fn());
+    this.queue = [];
+  }
+
+  _getRuntime(el) {
+    const ctx = this;
+    return {
+      el,
+      class: {
+        add: (cls) => el.classList.add(cls),
+        remove: (cls) => el.classList.remove(cls),
+      },
+      q(selector) {
+        return wrap(ctx.root.querySelectorAll(selector));
+      },
+      parent: {
+        data: {
+          set: (key, value) => {
+            ctx.store[key] = value;
+            ctx.root.setAttribute(`data-${key}`, value); // ✨ update DOM attribute
+          },
+          get: (key) => ctx.store[key],
+        },
+      },
+    };
+  }
 }
 
-window.Recipe = Recipe;
+// Utility for working with multiple DOM elements
+function wrap(elements) {
+  return {
+    class: {
+      add: (cls) => elements.forEach((el) => el.classList.add(cls)),
+      remove: (cls) => elements.forEach((el) => el.classList.remove(cls)),
+    },
+    eq(i) {
+      const el = elements[i];
+      return {
+        class: {
+          add: (cls) => el?.classList.add(cls),
+          remove: (cls) => el?.classList.remove(cls),
+        },
+      };
+    },
+  };
+}
